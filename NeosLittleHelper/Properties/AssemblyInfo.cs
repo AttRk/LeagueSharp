79fb8de80b26dc690b8d9ad7d4aacec1a1cbb8ec
Saniasharp <saniabol@hotmail.com>
2014-11-22 09:51:51 +0200

    ss

--- NeosLittleHelper/Properties/AssemblyInfo.cs
+++ NeosLittleHelper/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tracker")]
+[assembly: AssemblyTitle("Neos Little Helper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tracker")]
+[assembly: AssemblyProduct("Neos Little Helper")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
